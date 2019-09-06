c0453f02c8bd65d992f426f878b2c63fd3ed50e5
Tkachenko Vladislav <webkadiz@gmail.com>
2019-09-06 15:11:54 +0300

    add time in GUI

--- frontend/src/components/TenderList/TenderList.js
+++ frontend/src/components/TenderList/TenderList.js
@@ -3,10 +3,15 @@ import { TenderContext } from '@src/TenderContext'
 
 import { List, ListItem, ListItemText, ListItemPrimaryText, ListItemSecondaryText, ListItemMeta, ListDivider } from '@rmwc/list'
 import { IconButton } from '@rmwc/icon-button'
-import {Link} from 'react-router-dom'
+import { Ripple} from '@rmwc/ripple'
+import { Link } from 'react-router-dom'
 
 export default class TenderList extends Component {
   static contextType = TenderContext
+
+  computeTime = tender => {
+    return String(new Date(new Date(tender.tenderTimeEnd) - tender.tenderSecondsBeforeEnd).toLocaleString())
+  }
 
   render() {
     const { tenders, setEditableTender } = this.context
@@ -14,26 +19,31 @@ export default class TenderList extends Component {
 
     return (
       <List twoLine>
-        {tenders.map(({tenderName}, index) => (
+        {tenders.map((tender, index) => (
           <Fragment key={index}>
             <ListItem ripple='unbounded'>
              
               <ListItemText>
-                <ListItemPrimaryText>{tenderName}</ListItemPrimaryText>
-                <ListItemSecondaryText>45</ListItemSecondaryText>
+                <ListItemPrimaryText>{tender.tenderName}</ListItemPrimaryText>
+                <ListItemSecondaryText>{this.computeTime(tender)}</ListItemSecondaryText>
               </ListItemText>
-              
+
               <ListItemMeta
                 icon={
                   <>
-                    <IconButton icon='build' onClick={() => {}}/>
-                    <Link to="editing-tender"><IconButton icon='edit' onClick={setEditableTender.bind(null, tenderName)}/></Link>
-                    <IconButton icon='close' onClick={requestDeleteTender.bind(null, tenderName)}/>
+                    <Link to="editing-tender"><IconButton icon='edit' onClick={setEditableTender.bind(null, tender.tenderName)}/></Link>
+                    <IconButton icon='close' onClick={requestDeleteTender.bind(null, tender.tenderName)}/>
                   </>
                 }
               />
             </ListItem>
             <ListDivider />
+            <Ripple>
+              <div style={{display: 'flex'}}>
+                <div>123</div>
+                <IconButton icon='close' />
+              </div>
+            </Ripple>
           </Fragment>
         ))}
       </List>
